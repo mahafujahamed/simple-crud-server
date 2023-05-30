cf8e86cac5d0099b615c95dc3b9f6c81e43491c4
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-31 02:08:06 +0600

    simple crud server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = requre('express');
+const cors = require('cors');   // CORS = Cross Origin Resource Sharing
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://mahafujahamed068:yMdvLtaUUppeY2FJ@cluster0.blgp36b.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Simple CRUD is Running')
+})
+
+// mahafujahamed068
+// yMdvLtaUUppeY2FJ
+
+app.listen(port, () => {
+    console.log(`Simple Crud is Running on Port: ${port}`)
+})
