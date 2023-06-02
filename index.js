2241001919f76a470cb77e91063c92be38362610
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-06-02 18:08:01 +0600

    added post data

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
